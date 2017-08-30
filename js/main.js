451f2c511b2e189ff3637dad818402ef6320aa03
prathan1234 <prathan.psong@gmail.com>
2017-08-30 22:07:55 +0700

    Add Caps Lock button.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 function main() {
     var shift = false;
+    var caps = false;
 
     $("td").hover(function () {
         $(this).css('cursor','pointer');
@@ -14,20 +15,39 @@ function main() {
                 $(this).css('background-color', '#FFFFFF');
                 shift = false;
             } else {
-                $(this).css('background-color','#DDDDDD');
+                $(this).css('background-color', '#DDDDDD');
                 shift = true;
             }
-        } else if (input == "  Del") {
+        }
+        else if (input == "  Del") {
             var input = $(".display-screen").text();
             var lastChar = input.length - 1;
             $(".display-screen").text(input.substring(0, lastChar));
-        } else {
+        }
+        else if (input == "Caps") {
+            if (caps == true) {
+                $(this).css('background-color', '#FFFFFF');
+                caps = false;
+            } else {
+                $(this).css('background-color', '#DDDDDD');
+                caps = true;
+            }
+        }
+        else {
             $("td.shift").css('background-color','#FFFFFF');
+
             if (shift == true) {
                 input = input.toUpperCase();
             } else {
                 input = input.toLowerCase();
             }
+
+            if (caps == true) {
+                input = input.toUpperCase();
+            } else {
+                input = input.toLowerCase();
+            }
+
             var output = $(".display-screen").text() + input;
             $(".display-screen").text(output);
             shift = false;
